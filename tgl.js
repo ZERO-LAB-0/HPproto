21920697cb09f2160de33c4334dddf251d697fc7
ZERO-LAB-0 <win10.rei@outlook.jp>
2025-02-13 23:39:27 +0900

    Add files via upload

--- tgl.js
+++ tgl.js
@@ -1,6 +1,6 @@
-(function(){
-('.Toggle').click(function() {
-(this).toggleClass('active');
-('.menu').toggleClass('active');
- });
+const tgl = document.querySelector('#js-tgl'); //js-hamburgerの要素を取得し、変数hamに格納
+const menu = document.querySelector('#js-menu'); //js-navの要素を取得し、変数navに格納
+tgl.addEventListener('click', function () { //ハンバーガーメニューをクリックしたら
+	tgl.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
+	menu.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
 });
